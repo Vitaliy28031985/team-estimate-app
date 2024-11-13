4f7cd1d8b478023f6e2075735bc3feba216255dc
Vitaliy28031985 <vitaliy_piddubchak@ukr.net>
2024-11-13 23:02:49 +0200

    add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const { PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Estimate app')
+    .setDescription('api for estimate app')
+    .setVersion('1.0')
+    .addTag('estimate')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, documentFactory);
   await app.listen(PORT);
 }
 bootstrap();
