c9bbf99356762c1d96e36c6d17bddf579d73328d
Vitaliy28031985 <vitaliy_piddubchak@ukr.net>
2024-11-25 10:23:19 +0200

    add counter general

--- backend/src/projects/materials/materials.module.ts
+++ backend/src/projects/materials/materials.module.ts
@@ -7,12 +7,17 @@ import {
   ProjectSchema,
 } from 'src/mongo/schemas/project/project.schema';
 import { PositionsService } from '../positions/positions.service';
+import { User, UserSchema } from 'src/mongo/schemas/user/user.schema';
+import { Price, PriceSchema } from 'src/mongo/schemas/price.schema';
+import { SettingProjectService } from '../setting-project/setting.project.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Project.name, schema: ProjectSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: Price.name, schema: PriceSchema }]),
   ],
   controllers: [MaterialsController],
-  providers: [MaterialsService, PositionsService],
+  providers: [MaterialsService, PositionsService, SettingProjectService],
 })
 export class MaterialsModule {}
