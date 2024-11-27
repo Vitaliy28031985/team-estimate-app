e8a04e23a6310c1884e07f53953cafdbcf3d7eb9
roLLlyTHuK <71319633+roLLlyTHuK@users.noreply.github.com>
2024-11-27 23:14:13 +0200

    add next-auth lib, providers, api rout, change layout and page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,20 @@
+import GoogleAuthBtn from "./components/icons/googleAuthBtn";
 import Logo from "./components/icons/Logo/Logo";
 import Pencil from "./components/icons/Pencil/Pencil";
 
 export default function Home() {
   return (
-   
+
     <>Start estimate app
       <div className="bg-two">
         <div>
-         <Logo width={"74"} height={"74"} /> 
+          <Logo width={"74"} height={"74"} />
         </div>
         <div className="pencil"> <Pencil width={"24"} height={"24"} /></div>
-        
-       
+        <GoogleAuthBtn />
+
       </div>
-   
+
     </>
   );
 }
