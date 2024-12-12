fe167466eab20c421a6d9d79526bf2fa6d780f0b
Vitaliy28031985 <vitaliy_piddubchak@ukr.net>
2024-12-12 10:22:36 +0200

    correct verify code

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,20 +1,24 @@
 import GoogleAuthBtn from "./components/icons/googleAuthBtn";
 import Logo from "./components/icons/Logo/Logo";
 import Pencil from "./components/icons/Pencil/Pencil";
-
+import { TrashIcon, PrinterIcon } from "@heroicons/react/24/solid";
 export default function Home() {
   return (
-
-    <>Start estimate app
+    <>
+      Start estimate app
       <div className="bg-two">
         <div>
           <Logo width={"74"} height={"74"} />
         </div>
-        <div className="pencil"> <Pencil width={"24"} height={"24"} /></div>
+        <div className="pencil">
+          {" "}
+          <Pencil width={"24"} height={"24"} />
+        </div>
+        <p className="text-red-300">jhkjhkjh</p>
         <GoogleAuthBtn />
-
+        <PrinterIcon className="size-6 text-gray-800 hover:text-gray-500" />
+        <TrashIcon className="size-6 text-gray-800 hover:text-gray-500" />
       </div>
-
     </>
   );
 }
